46cfdba47a44f2edb32971b168e18e28b53092be
Ostin63 <Ostin@63users.noreply.githab.com>
2018-11-23 13:48:16 +0400

    написал функцию проверки остатка времени до выполнения задачи

--- functions.php
+++ functions.php
@@ -25,3 +25,12 @@ function countTasks($tasks_list, $val) {
     }
     return $tasksAmount;
 }
+//функция проверки остатка времени до выполнения задачи
+function isTaskImportant($taskDate, $importantHours) {
+    if ($taskDate == null) return  false;
+    $seconds_in_hour = 3600;
+    $ts = time();
+    $end_ts= strtotime($taskDate);
+    $ts_diff = $end_ts - $ts;
+    return floor($ts_diff / $seconds_in_hour) <= $importantHours;
+}
