fd87fe4d8ad0cb794cb1e58a3f3db16b62b23e87
Ostin63 <Ostin@63users.noreply.githab.com>
2018-11-26 11:30:40 +0400

    изменение таблиц

--- schema.sql
+++ schema.sql
@@ -4,19 +4,30 @@ DEFAULT COLLATE utf8_general_ci;
 
 USE doingsdone;
 
-CREATE TABLE categories (
-`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE users (
+`id` INT NOT AUTO_INCREMENT,
+`date_creation` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+`email` VARCHAR(255) NOT NULL,
 `name` VARCHAR(255) NOT NULL,
+`password` VARCHAR(64) NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY `email`(`email`)
+);
+
+CREATE TABLE projects (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(64) NOT NULL,
 PRIMARY KEY (`id`),
 UNIQUE KEY `name`(`name`)
 );
 
 CREATE TABLE tasks (
 `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-`name` VARCHAR(255) NOT NULL,
 `date_creation` TIMESTAMP,
 `date_completion` TIMESTAMP,
 `date_term` TIMESTAMP,
+`name` VARCHAR(255) NOT NULL,
+`file` VARCHAR(255),
 `category_id` INT NOT NULL,
 `done` BIT NOT NULL DEFAULT b'0',
 PRIMARY KEY (`id`),
@@ -24,12 +35,4 @@ UNIQUE KEY `name`(`name`),
 KEY `category`(`category_id`)
 );
 
-CREATE TABLE user (
-`id` INT UNSIGNED NOT AUTO_INCREMENT,
-`date_creation` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ,
-`email` VARCHAR(255) NOT NULL,
-`name` VARCHAR(255) NOT NULL,
-`password` VARCHAR(64) NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE KEY `email`(`email`)
-);
+
