6fd2037cd0a0be17c28cd9731c08c1c913169e90
Ostin63 <Ostin@63users.noreply.githab.com>
2018-11-26 10:07:28 +0400

    добавил пользователя, таблице - задачи, добавил поля

--- schema.sql
+++ schema.sql
@@ -11,13 +11,25 @@ PRIMARY KEY (`id`),
 UNIQUE KEY `name`(`name`)
 );
 
-CREATE TABLE tasks_list (
+CREATE TABLE tasks (
 `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 `name` VARCHAR(255) NOT NULL,
-`date` TIMESTAMP,
+`date_creation` TIMESTAMP,
+`date_completion` TIMESTAMP,
+`date_term` TIMESTAMP,
 `category_id` INT NOT NULL,
 `done` BIT NOT NULL DEFAULT b'0',
 PRIMARY KEY (`id`),
 UNIQUE KEY `name`(`name`),
 KEY `category`(`category_id`)
+);
+
+CREATE TABLE user (
+`id` INT UNSIGNED NOT AUTO_INCREMENT,
+`date_creation` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ,
+`email` VARCHAR(255) NOT NULL,
+`name` VARCHAR(255) NOT NULL,
+`password` VARCHAR(64) NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY `email`(`email`)
 );
